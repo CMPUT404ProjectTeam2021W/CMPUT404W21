c5004d88202b10448ba06df30f4c7d6b2b5690fd
Nana Andoh <nanaegyir2000@hotmail.com>
2021-03-02 13:32:57 -0700

    added basic html and css templates for index

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('socialdist/', include('socialdist.urls')),
+    path('', include('socialdist.urls')),
     path('admin/', admin.site.urls),
 ]
