9a5fd1e7bc362a0f9908721438c8a85076255dd3
Nana Andoh <nanaegyir2000@hotmail.com>
2021-03-01 19:18:44 -0700

    added author model

--- mysite/socialdist/models.py
+++ mysite/socialdist/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=200)
+
+class Author(models.Model):
+    githublink = models.URLField()
+    username = models.CharField(max_length=20)
+    
