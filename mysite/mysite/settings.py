e7484ec40b1bc498a15b29583e08bd92846dd62b
Jason Robock <jrobock@ualberta.ca>
2021-03-04 15:03:19 -0700

    Upload images issue (#51)
    * nearly? working image upload
    
    * fixed user issue

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,6 +126,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static/"),
 )
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 LOGIN_REDIRECT_URL = 'feed'
 LOGOUT_REDIRECT_URL = 'index'
 django_on_heroku.settings(locals()) # bottom of the file
--- mysite/socialdist/models.py
+++ mysite/socialdist/models.py
@@ -3,8 +3,13 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=200)
+    USERNAME_FIELD = 'name'
+    REQUIRED_FIELDS = []
 
 class Author(models.Model):
     githublink = models.URLField()
     username = models.CharField(max_length=20)
-    
+
+class Image(models.Model):
+    name = models.CharField(max_length=50)
+    new_image = models.ImageField(upload_to='images/') 
