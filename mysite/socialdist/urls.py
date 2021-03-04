8bf1d9f67a67e92818823a73027c56d2eef41c20
orackle <debangan@ualberta.ca>
2021-03-03 23:36:26 -0700

    changes for feed: update from dev

--- mysite/socialdist/urls.py
+++ mysite/socialdist/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
   path('', views.index, name='index'),
   path('signup/', views.SignUpView.as_view(), name='signup'),
+  path('feed/', views.feed, name='feed'),
   path('', include('django.contrib.auth.urls'))
 
 ]
