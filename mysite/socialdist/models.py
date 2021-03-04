6358ff14870e1fb8a265a2dadf81bda3de9b0f61
Jason Robock <jrobock@ualberta.ca>
2021-03-04 14:40:06 -0700

    fixed user issue

--- mysite/socialdist/models.py
+++ mysite/socialdist/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=200)
+    USERNAME_FIELD = 'name'
+    REQUIRED_FIELDS = []
 
 class Author(models.Model):
     githublink = models.URLField()
