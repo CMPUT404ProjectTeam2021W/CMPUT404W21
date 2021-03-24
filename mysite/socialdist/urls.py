19a5327d0cc8c29717e5ecbed3dc9c90a94b0132
orackle <debangan@ualberta.ca>
2021-03-23 22:02:48 -0600

    changed profile page to be restful, every username now hyperlinked to individual profile page, follow button on top can toggle between follow/following

--- mysite/socialdist/urls.py
+++ mysite/socialdist/urls.py
@@ -13,8 +13,9 @@ urlpatterns = [
   path('create_post/', views.create_post, name='create_post'),
   path('', include('django.contrib.auth.urls')),
   path('profile/', views.profile_posts, name='profile'),
-  path('user_settings/', views.user_settings, name = 'user_settings')
+  path('user_settings/', views.user_settings, name = 'user_settings'),
 
+  path('author/<str:author_id>/', views.author_profile, name='author_profile')
 ]
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
